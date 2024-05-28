497e8fdb817c4b7f370537dd75a5e3888ad70be7
Raul Pascual <raulpascual63@gmail.com>
2024-05-28 22:38:51 +0200

    - Added updateAppView - Added doc to Onboarding - Added Text Extension

---
+++ Tests/UIComponentsSnapshotTests/UpdateViewTests.swift
@@ -0,0 +1,20 @@
+//
+//  UpdateViewTests.swift
+//  
+//
+//  Created by Raul on 28/5/24.
+//
+
+import SwiftUI
+import XCTest
+import SnapshotTesting
+
+@testable import UIComponents
+
+final class UpdateViewTests: XCTestCase {
+    func testUpdateView() {
+        let view = exampleUpdateView()
+        assertSnapshot(of: view,
+                       as: .wait(for: 3, on: .image(precision: 1, layout: .device(config: .iPhone13Pro))))
+    }
+}
