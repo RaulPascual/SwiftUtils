b55b28e116cbedf766707306da0b3e7da8726043
Raul Pascual <raulpascual63@gmail.com>
2024-07-19 16:31:59 +0200

    Filtering and searching by method or url in network monitor

--- Sources/SwiftUtils/DebugView/HTTPMonitor/DebugView.swift
+++ Sources/SwiftUtils/DebugView/HTTPMonitor/DebugView.swift
@@ -8,28 +8,16 @@
 import SwiftUI
 
 struct DebugView: View {
-    let debugViewModel = DebugViewModel()
     @StateObject var debugHTTPS = DebugViewHTTPS.shared
     
     var body: some View {
         List {
             Section(header: Text("Welcome to Debug View").bold()) {
-                ForEach(debugViewModel.model, id: \.id) { item in
                     NavigationLink(destination: RequestsListView(list: debugHTTPS)) {
-                        Text(item.rawValue)
-                    }
+                        Text("HTTPS Response")
                 }
             }
         }
         .navigationTitle("Debug view")
-    }
-}
-
-struct DebugViewModel {
-    let model: [DebugType] = DebugType.allCases
-    
-    enum DebugType: String, CaseIterable {
-        var id: Int { hashValue }
-        case httpsResponse = "HTTPS Response"
     }
 }
